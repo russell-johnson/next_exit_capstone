ecf3b0252ef894fd67d551475ee306b763b062fe
Todd Utterback <todd.j.utterback@gmail.com>
2016-04-11 16:50:23 -0600

    submit button function

--- app/assets/javascripts/road_trips.js.jsx
+++ app/assets/javascripts/road_trips.js.jsx
@@ -1,15 +1,30 @@
 class RoadTrips extends React.Component {
   constructor(props) {
     super(props)
-    this.state = {origin: '', destination: ''}
+    this.state = {origin: "Provo", destination: "Dallas"}
+    this.getDirections = this.getDirections.bind(this);
   }
 
-  getDirections(){
+  getDirections(e){
+    e.preventDefault()
+    let origin = "";
+    let destination = "";
 
+    if (this.refs.origin.value == "") {
+      origin = this.state.origin;
+    }else{
+      origin = this.refs.origin.value;
+    }
+    if (this.refs.destination.value == "") {
+      destination = this.state.destination;
+    }else{
+      destination = this.refs.destination.value;
+    }
+    this.setState({origin: origin, destination: destination})
   }
 
   render() {
-    let url = `https://www.google.com/maps/embed/v1/directions?key=${this.props.ek}&origin=Provo+Utah&destination=Dallas+Texas&mode=driving`
+    let url = `https://www.google.com/maps/embed/v1/directions?key=${this.props.ek}&origin=${this.state.origin}&destination=${this.state.destination}&mode=driving`
     return(
       <div className='container'>
         <iframe
@@ -17,12 +32,15 @@ class RoadTrips extends React.Component {
           height="500"
           src= {url}>
         </iframe>
-        <div className='row'>
-          <div className='col m6'>
-            <input className='field' type='text' placeholder='origin' ref='origin'></input>
-            <input className='field' type='text' placeholder='destination' ref='destination'></input>
+        <form onSubmit={this.getDirections}>
+          <div className='row'>
+            <div className='col m6'>
+              <input className='field' type='text' placeholder='origin' ref='origin'></input>
+              <input className='field' type='text' placeholder='destination' ref='destination'></input>
+              <button className='btn-large' type='submit'>NEXT EXIT</button>
+            </div>
           </div>
-        </div>
+        </form>
       </div>
     );
   }
