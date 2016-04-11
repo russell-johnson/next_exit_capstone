98f1d67adecc0b19cdab2dc58b2169f669f5d01d
Russell Johnson <russellmax86@gmail.com>
2016-04-11 17:39:54 -0600

    added waypoints to react component

--- app/assets/javascripts/road_trips.js.jsx
+++ app/assets/javascripts/road_trips.js.jsx
@@ -1,7 +1,7 @@
 class RoadTrips extends React.Component {
   constructor(props) {
     super(props)
-    this.state = {origin: "Provo", destination: "Dallas"}
+    this.state = {origin: "Provo", destination: "Dallas", waypoints: "provo"}
     this.getDirections = this.getDirections.bind(this);
   }
 
@@ -9,6 +9,7 @@ class RoadTrips extends React.Component {
     e.preventDefault()
     let origin = "";
     let destination = "";
+    let waypoints = "";
 
     if (this.refs.origin.value == "") {
       origin = this.state.origin;
@@ -20,11 +21,16 @@ class RoadTrips extends React.Component {
     }else{
       destination = this.refs.destination.value;
     }
-    this.setState({origin: origin, destination: destination})
+    if (this.refs.waypoints.value == "") {
+      waypoints = this.state.waypoints;
+    }else{
+      waypoints = this.refs.waypoints.value;
+    }
+    this.setState({origin: origin, destination: destination, waypoints: waypoints})
   }
 
   render() {
-    let url = `https://www.google.com/maps/embed/v1/directions?key=${this.props.ek}&origin=${this.state.origin}&destination=${this.state.destination}&mode=driving`
+    let url = `https://www.google.com/maps/embed/v1/directions?key=${this.props.ek}&origin=${this.state.origin}&destination=${this.state.destination}&mode=driving&waypoints=${this.state.waypoints}`
     return(
       <div className='container'>
         <iframe
@@ -37,6 +43,7 @@ class RoadTrips extends React.Component {
             <div className='col m6'>
               <input className='field' type='text' placeholder='origin' ref='origin'></input>
               <input className='field' type='text' placeholder='destination' ref='destination'></input>
+              <input className='field' type='text' placeholder='Stops: optional' ref='waypoints'></input>
               <button className='btn-large' type='submit'>NEXT EXIT</button>
             </div>
           </div>
