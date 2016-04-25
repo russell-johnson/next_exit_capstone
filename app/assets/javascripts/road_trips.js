1c5de15a89d36f20f3aca245aa5e34db2fa8ae36
jsquire4 <jacobmsquire@yahoo.com>
2016-04-25 13:47:07 -0600

    finalized input on index

--- app/assets/javascripts/road_trips.js
+++ app/assets/javascripts/road_trips.js
@@ -612,8 +612,8 @@ Number.prototype.toBrng = function () {
     distance = .25 * 1.609344;
 
     var request = {
-      origin: document.getElementById("from").value,
-      destination: document.getElementById("to").value,
+      origin: document.getElementById("to").value,
+      destination: document.getElementById("from").value,
       travelMode: google.maps.DirectionsTravelMode.DRIVING
     }
 
