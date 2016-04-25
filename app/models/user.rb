db758210399e9093e4afcb02df61878943716b3c
Parker Smith <linuxlovr1@gmail.com>
2016-04-25 16:54:36 -0600

    omniauth

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,7 +11,7 @@ class User < ActiveRecord::Base
   def self.from_omniauth(facebook_info)
     where(provider: facebook_info.provider, uid: facebook_info.uid).first_or_create do |user|
       user.email = facebook_info.info.email 
-      user.password = Devise.friendly_token
+      user.password = Devise.friendly_token 
     end
   end
 end
