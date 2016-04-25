adcd26e2f0576ee70af7b3eef886897642ed1367
Russell Johnson <russellmax86@gmail.com>
2016-04-25 11:47:54 -0600

    put yml file in developmnet

--- config/initializers/google_maps_api.rb
+++ config/initializers/google_maps_api.rb
@@ -1,7 +1,9 @@
-begin
-  YAML.load(File.read('config/google_map_api.yml')).each do |key, value|
-    ENV[key] = value
+if Rails.env.development?
+  begin
+    YAML.load(File.read('config/google_map_api.yml')).each do |key, value|
+      ENV[key] = value
+    end
+  rescue
+    raise 'No google_map_api file found in config dir!'
   end
-rescue
-  raise 'No google_map_api file found in config dir!'
 end
